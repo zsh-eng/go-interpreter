4913371c4e5cd436f38c1b2dcf37641a3ed2c495
sheng <chengzhisheng2001@gmail.com>
2023-12-23 04:34:13 +0800

    feat: add helper function for checking IDENT

--- internal/token/token.go
+++ internal/token/token.go
@@ -32,3 +32,17 @@ type Token struct {
 	Type    TokenType
 	Literal string // To be able to access the literal value later on
 }
+
+// Keywords in the Monkey programming language
+var keywords = map[string]TokenType{
+	"fn": FUNCTION,
+	"let": LET,
+}
+
+// Check if the identifier is a keyword
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
